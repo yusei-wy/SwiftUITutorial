48beabba96053c7f69825856b1d8524f9b803cfc
yusei-wy <yusei.kasa@gmail.com>
2024-07-09 20:55:35 +0900

    First commit

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Text("Swift")
+        .font(.system(size: 100))
+        .fontWeight(.black)
+        .foregroundStyle(
+          LinearGradient(
+            colors: [.pink, .purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
     }
+    .padding()
+  }
 }
 
-#Preview {
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
     ContentView()
+  }
 }
+
+// #Preview {
+//     ContentView()
+// }
